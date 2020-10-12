8653c1004af5213a844a401cffbb60a05865f894
Oleksii Tromsa <tromsaalex@gmail.com>
2020-10-12 19:13:51 +0300

    Delete unused import

--- src/main/java/com/dev/cinema/model/Order.java
+++ src/main/java/com/dev/cinema/model/Order.java
@@ -3,7 +3,6 @@ package com.dev.cinema.model;
 import java.time.LocalDateTime;
 import java.util.List;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
