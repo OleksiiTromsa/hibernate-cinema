7f91b2f8f04a9d0a623af0915951065c23198883
Oleksii Tromsa <tromsaalex@gmail.com>
2020-10-16 08:49:19 +0300

    Add GenericDao and logger

---
+++ src/main/java/com/dev/cinema/dao/impl/GenericDaoImpl.java
@@ -0,0 +1,35 @@
+package com.dev.cinema.dao.impl;
+
+import com.dev.cinema.dao.GenericDao;
+import com.dev.cinema.exceptions.DataProcessingException;
+import com.dev.cinema.util.HibernateUtil;
+import org.apache.log4j.Logger;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class GenericDaoImpl<T> implements GenericDao<T> {
+    private static final Logger logger = Logger.getLogger(GenericDaoImpl.class);
+
+    @Override
+    public T add(T item) {
+        Transaction transaction = null;
+        Session session = null;
+        try {
+            session = HibernateUtil.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.save(item);
+            transaction.commit();
+            logger.info("Entity " + item + " successfully added to the DB");
+            return item;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new DataProcessingException("Can't insert entity " + item, e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+}
--- src/main/java/com/dev/cinema/dao/impl/ShoppingCartDaoImpl.java
+++ src/main/java/com/dev/cinema/dao/impl/ShoppingCartDaoImpl.java
@@ -6,34 +6,15 @@ import com.dev.cinema.lib.Dao;
 import com.dev.cinema.model.ShoppingCart;
 import com.dev.cinema.model.User;
 import com.dev.cinema.util.HibernateUtil;
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
 @Dao
-public class ShoppingCartDaoImpl implements ShoppingCartDao {
-    @Override
-    public ShoppingCart add(ShoppingCart shoppingCart) {
-        Session session = null;
-        Transaction transaction = null;
-        try {
-            session = HibernateUtil.getSessionFactory().openSession();
-            transaction = session.beginTransaction();
-            session.save(shoppingCart);
-            transaction.commit();
-            return shoppingCart;
-        } catch (Exception e) {
-            if (transaction != null) {
-                transaction.rollback();
-            }
-            throw new DataProcessingException("Cannot insert ShoppingCart entity "
-                    + shoppingCart, e);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
-        }
-    }
+public class ShoppingCartDaoImpl extends GenericDaoImpl<ShoppingCart>
+        implements ShoppingCartDao {
+    private static final Logger logger = Logger.getLogger(ShoppingCartDaoImpl.class);
 
     @Override
     public ShoppingCart getByUser(User user) {
@@ -55,6 +36,8 @@ public class ShoppingCartDaoImpl implements ShoppingCartDao {
             transaction = session.beginTransaction();
             session.update(shoppingCart);
             transaction.commit();
+            logger.info("Shopping cart " + shoppingCart
+                    + " was updated successfully");
         } catch (Exception e) {
             if (transaction != null) {
                 transaction.rollback();
