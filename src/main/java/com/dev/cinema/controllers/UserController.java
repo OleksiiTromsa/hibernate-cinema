957e7e4e9c61bde62bee9dc86a06347c03d7b2e5
Oleksii Tromsa <tromsaalex@gmail.com>
2020-11-02 11:08:24 +0200

    Fix UserController.getByEmail()

--- src/main/java/com/dev/cinema/controllers/UserController.java
+++ src/main/java/com/dev/cinema/controllers/UserController.java
@@ -3,10 +3,9 @@ package com.dev.cinema.controllers;
 import com.dev.cinema.mappers.UserMapper;
 import com.dev.cinema.model.dto.UserResponseDto;
 import com.dev.cinema.service.UserService;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -21,9 +20,7 @@ public class UserController {
     }
 
     @GetMapping("/by-email")
-    public UserResponseDto getByEmail(Authentication authentication) {
-        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
-        String email = userDetails.getUsername();
+    public UserResponseDto getByEmail(@RequestParam String email) {
         return mapper.userToDto(service.findByEmail(email));
     }
 }
